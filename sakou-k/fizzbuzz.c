1a69b14a23404acb04328e24233ff222eb28ab37
sakou-k <sako.koji@po.ntts.co.jp>
2012-12-21 19:44:55 +0900

    sakou-k first commit
    sakou-k commit

---
+++ sakou-k/fizzbuzz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int
+main(int ac,char** av)
+{
+	int i;
+	for(i=1;i<255;i++){
+		if( i % 3 == 0 && i % 5 == 0){
+			printf("fizz buzz\n");
+		} else if ( i % 3 == 0 ){
+			printf("fizz\n");
+		} else if ( i % 5 == 0 ){
+			printf("buzz\n");
+		} else {
+			printf("%d\n",i);
+		}
+	}
+}
